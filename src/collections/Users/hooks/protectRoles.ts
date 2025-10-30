0bcb037b975bb939d5323a6f1a59ceb6d472d988
Marycarmen <marycarmen.rivo@gmail.com>
2025-10-30 10:28:25 -0600

    Actualización de colección Users y creación de hook protectRoles

---
+++ src/collections/Users/hooks/protectRoles.ts
@@ -0,0 +1,19 @@
+import type { FieldHook } from 'payload'
+import type { User } from '@/payload-types'
+
+export const protectRoles: FieldHook<{ id: string } & User> = ({ data, req }) => {
+  //TODO:
+  /* if (req.user?.collection === 'users') {
+    const isAdmin = req.user?.roles?.includes('admin')
+
+    if (!isAdmin) {
+      if (!data?.roles?.includes('editor')) {
+        return ['user']
+      }
+    }
+
+    const userRoles = new Set(data?.roles || [])
+    userRoles.add('user')
+    return [...userRoles]
+  } */
+}
