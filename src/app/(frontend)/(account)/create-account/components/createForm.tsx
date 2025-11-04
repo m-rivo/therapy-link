5f3cf74d5fe3abdd29e13151a70f0f3a77200708
Marycarmen <marycarmen.rivo@gmail.com>
2025-11-04 15:49:04 -0600

    Cambio de inputs de formulario de registro

--- src/app/(frontend)/(account)/create-account/components/createForm.tsx
+++ src/app/(frontend)/(account)/create-account/components/createForm.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import React, { ReactElement, useState } from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { create, CreateResponse } from '../actions/create'
-import Link from 'next/link'
 import { FormContainer } from '@/components/CustomerForm/FormContainer'
-import Input from '@/components/CustomerForm/Input'
+//import Input from '@/components/CustomerForm/Input'
 import SubmitButton from '@/components/CustomerForm/SubmitButton'
+import { Field, FieldSet, FieldGroup, FieldLabel } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
 
 export default function CreateForm(): ReactElement {
   // create loading states using the useState hook. The default should be false.
@@ -23,25 +25,32 @@ export default function CreateForm(): ReactElement {
 
     const formData = new FormData(event.currentTarget)
 
-    const email = formData.get('email') as string
-    const password = formData.get('password') as string
-    const confirmPassword = formData.get('confirmPassword') as string
-    const firstName = formData.get('firstName') as string
-    const lastName = formData.get('lastName') as string
-    //const username = formData.get('username') as string
+    const email = formData.get('correo') as string
+    const password = formData.get('contrasena') as string
+    const confirmPassword = formData.get('confirm-contrasena') as string
+    const firstName = formData.get('nombre') as string
+    const lastName = formData.get('apellido') as string
+    //const username = formData.get('usuario') as string
 
     if (password !== confirmPassword) {
-      setError('Passwords do not match')
+      setError('Las contraseñas no coinciden')
       setIsLoading(false)
       return
     }
+
+    console.log({
+      email,
+      password,
+      lastName,
+      firstName,
+    })
 
     const result: CreateResponse = await create({
       email,
       password,
       lastName,
       firstName,
-      /*  username */
+      //username
     })
 
     setIsLoading(false)
@@ -54,28 +63,40 @@ export default function CreateForm(): ReactElement {
   }
 
   return (
-    <FormContainer heading={'Create an account'}>
-      <form className={`flex flex-col gap-4`} onSubmit={handleSubmit}>
-        <div className={`flex flex-row flex-1/2 gap-2`}>
-          <Input label={`First Name`} name={`firstName`} type={'text'} />
-          <Input label={`Last Name`} name={`lastName`} type={'text'} />
-        </div>
-        <Input label={'Email'} type={'email'} name={'email'} />
-        <Input label={'Password'} type={'password'} name={'password'} />
-        <Input
-          label={'Confirm Password'}
-          type={'password'}
-          name={'confirmPassword'}
-          placeholder={'Confirm your password'}
-        />
-        {error && <div className={`text-red-400`}>{error}</div>}
-        <SubmitButton loading={isLoading} text={`Create account`} />
+    <FormContainer heading="Crea una cuenta">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <FieldSet>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="nombre">Nombre</FieldLabel>
+              <Input id="nombre" name="nombre" type="text" required />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="apellido">Apellido</FieldLabel>
+              <Input id="apellido" name="apellido" type="text" required />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="correo">Correo</FieldLabel>
+              <Input id="correo" name="correo" type="email" required />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="contrasena">Contraseña</FieldLabel>
+              <Input id="contrasena" name="contrasena" type="password" required />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="confirm-contrasena">Confirma Contraseña</FieldLabel>
+              <Input id="confirm-contrasena" name="confirm-contrasena" type="password" required />
+            </Field>
+          </FieldGroup>
+        </FieldSet>
+        {error && <div className="text-red-400">{error}</div>}
+        <SubmitButton loading={isLoading} text="Crear cuenta" />
       </form>
-      <div className={`mt-4`}>
-        <p className={`text-sm text-emerald-950/50`}>
-          Already have an account?{' '}
-          <Link className={`underline underline-offset-4`} href={`/login`}>
-            Login here.
+      <div className="mt-4">
+        <p className="text-sm">
+          Ya tienes una cuenta?{' '}
+          <Link className="underline underline-offset-4" href="/login">
+            Inicia sesión aquí
           </Link>
         </p>
       </div>
--- src/app/(frontend)/(account)/create-account/page.tsx
+++ src/app/(frontend)/(account)/create-account/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import CreateForm from './components/createForm'
-import { getUser } from '@/app/(auth)/actions/getUser'
+import { getUser } from '@/app/(frontend)/(private)/actions/getUser'
 import { redirect } from 'next/navigation'
 
 export default async function Page(): Promise<React.ReactElement> {
