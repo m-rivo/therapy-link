2e8ebddff643dd85b71818ac24550834749c0ca3
Marycarmen <marycarmen.rivo@gmail.com>
2025-11-05 17:04:51 -0600

    Mejora de estilo y traducción de correos de verificación de correo y cambio de contraseña

--- src/collections/Customers/config.ts
+++ src/collections/Customers/config.ts
@@ -7,19 +7,19 @@ export const Customers: CollectionConfig = {
     tokenExpiration: 12 * 60 * 60,
     verify: {
       generateEmailSubject: (args) => {
-        return `Hey ${args?.user?.firstName ? args?.user.firstName : args?.user.email}, verify your email address!`
+        return `Hola ${args?.user?.firstName ? args?.user.firstName : args?.user.email}, verifica tu correo!`
       },
       generateEmailHTML: (args) => {
-        return `<div><h1>Hey ${args.user?.firstName ? args.user.firstName : args.user.email}!</h1><br /><p>Verify your email address by going to <a href="${getServerSideURL()}/verify?token=${args.token}">${getServerSideURL()}/verify?token=${args.token}</a></p></div>`
+        return `<!DOCTYPE html><html lang="es"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Verifica tu correo</title></head><body style="font-family: Verdana, sans-serif; text-align: center; margin: 0; background-color: #f4f4f4;"><main style="padding: 0 16px;"><h1 style="color: navy; font-size: 2rem; margin-top: 80px;">Verifica tu correo</h1><p style="font-size: 1rem; color: #333;">Hola ${args.user?.firstName ? args.user.firstName : args.user.email},</p><p style="font-size: 1rem; color: #333;">Haz clic en el botón de abajo para verificar tu correo electrónico.</p><div style="margin-top: 40px;"><a href="${getServerSideURL()}/verify?token=${args.token}" style="padding: 16px; background-color: navy; color: white; text-decoration: none; font-size: 1rem; border-radius: 5px;">Verificar mi correo</a></div></main><footer style="margin-top: 80px; background-color: black; height: 100px; color: white; display: flex; justify-content: center; align-items: center;"><p style="font-size: 0.75rem;">Therapy Link &copy; Derechos Reservados</p></footer></body></html>`
       },
     },
     forgotPassword: {
       generateEmailSubject: (args) => {
         // we need to use `args` and conditional chaining here to prevent type issues
-        return `Hey ${args?.user?.firstName ? args?.user.firstName : args?.user.email}! Reset your password.`
+        return `Hola ${args?.user?.firstName ? args?.user.firstName : args?.user.email}! Reestablece tu contraseña.`
       },
       generateEmailHTML: (args) => {
-        return `<div><h1>Hey ${args?.user?.firstName ? args?.user.firstName : args?.user.email}!</h1><br /><p>You (or someone else) requested to reset your password. If this wasn't you, you can safely ignore this email. Otherwise, reset your password by going to ${getServerSideURL()}/password-reset?token=${args?.token}</p></div>`
+        return `<!DOCTYPE html><html lang="es"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Reestablece tu contraseña</title></head><body style="font-family: Verdana, sans-serif; text-align: center; margin: 0; background-color: #f4f4f4;"><main style="padding: 0 16px;"><h1 style="color: navy; font-size: 2rem; margin-top: 80px;">Reestablece tu contraseña</h1><p style="font-size: 1rem; color: #333;">Hola ${args?.user?.firstName ? args?.user.firstName : args?.user.email},</p><p style="font-size: 1rem; color: #333;">Tú (o alguien más) solicitaste reestablecer tu contraseña. Si no fuiste tú, simplemente ignora este correo. De lo contrario puedes reestablecer tu contraseña haciendo clic en el botón de abajo</p><div style="margin-top: 40px;"><a href="${getServerSideURL()}/password-reset?token=${args?.token}" style="padding: 16px; background-color: navy; color: white; text-decoration: none; font-size: 1rem; border-radius: 5px;">Reestablecer mi contraseña</a></div></main><footer style="margin-top: 80px; background-color: black; height: 100px; color: white; display: flex; justify-content: center; align-items: center;"><p style="font-size: 0.75rem;">Therapy Link &copy; Derechos Reservados</p></footer></body></html>`
       },
     },
     cookies: {
