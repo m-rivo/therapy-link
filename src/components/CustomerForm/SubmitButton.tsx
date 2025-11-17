328ae545410885d88873b1c49d432b439be30cb6
Marycarmen <marycarmen.rivo@gmail.com>
2025-11-17 14:50:48 -0600

    Agregación de props en submitButton

--- src/components/CustomerForm/SubmitButton.tsx
+++ src/components/CustomerForm/SubmitButton.tsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { LoaderCircle } from 'lucide-react'
 import { Button } from '../ui/button'
 
 export default function SubmitButton({
   loading,
   text,
+  type = 'submit',
+  variant = 'default',
+  ariaLabel = 'submit form',
+  onClick = undefined,
+  size = 'default',
 }: {
   loading: boolean
-  text: string
+  text: string | ReactNode
+  type?: 'submit' | 'button' | 'reset'
+  variant?: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link'
+  ariaLabel?: string
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  size?: 'default' | 'sm' | 'lg' | 'icon' | 'icon-sm' | 'icon-lg'
 }): React.ReactElement {
   return (
     <Button
-      type="submit"
+      type={type}
       className={loading ? 'cursor-not-allowed' : 'cursor-pointer'}
       disabled={loading}
+      variant={variant}
+      aria-label={ariaLabel}
+      onClick={type === 'submit' ? undefined : onClick}
+      size={size}
     >
       {text}
       <LoaderCircle className={`animate-spin ${loading ? 'inline-block' : 'hidden'}`} />
